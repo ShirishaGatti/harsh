f81aaf9b17fbc018d6846c3defc2e4762ce55c38
unknown <shirishagatti99@gmail.com>
2025-06-04 22:03:03 +0530

    fix:gitignore

--- src/main/java/com/backend/harsh/config/CorsGlobalFilter.java
+++ src/main/java/com/backend/harsh/config/CorsGlobalFilter.java
@@ -1,3 +1,29 @@
+//package com.backend.harsh.config;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.filter.CorsFilter;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@Configuration
+//public class CorsGlobalFilter {
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowedOrigins(List.of( "https://harshclinic-5smh.vercel.app"));
+//        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        config.setAllowedHeaders(Arrays.asList("*"));
+//        config.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
+//}
+
 package com.backend.harsh.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +41,13 @@ public class CorsGlobalFilter {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of( "https://harshclinic-5smh.vercel.app"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(Arrays.asList("*"));
-        config.setAllowCredentials(true);
+        // config.setAllowedHeaders(Arrays.asList("*"));
+        // config.setAllowCredentials(true);
+         config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true); // Allow cookies
+        config.setMaxAge(3600L); // Cache pre-flight for 1 hour
+        // source.registerCorsConfiguration("/**", config);
+        // return new CorsFilter(source);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
